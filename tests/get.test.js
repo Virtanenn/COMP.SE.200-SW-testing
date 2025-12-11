c2786b21564563a4ed59cc1610f372f644e59989
Timo Virtanen <timo.virtanen@tuni.fi>
2025-12-11 18:26:40 +0200

    tests for get function

--- tests/get.test.js
+++ tests/get.test.js
@@ -3,8 +3,18 @@ const { expect } = chai;
 
 import get from "../library/src/get.js";
 
-describe("Integration – Stub Tests", () => {
-  it("placeholder integration test", () => {
-    expect("ok").to.equal("ok");
+describe("get", () => {
+  it("basic tests for get", () => {
+    const object = { 'a': [{ 'b': { 'c': 3 } }] }
+    expect(get(object, 'a[0].b.c')).to.equal(3);
+    expect(get(object, 'a')).to.deep.equal([{ 'b': { 'c': 3 } }]);
+    expect(get(object, 'a.b.c', 'default')).to.deep.equal('default');
+    expect(get(object, 'a.b.c.c.e.s.')).to.deep.equal(undefined);
+  });
+  it("Test default value", () => {
+    const object = { 'a': [{ 'b': { 'c': 3 } }] }
+    expect(get(object, 'a.b.c', 'default')).to.deep.equal('default');
+    expect(get(object, 'a.b.c.c.e.s.')).to.deep.equal(undefined);
+    expect(get(object, null)).to.deep.equal(undefined);
   });
 });
