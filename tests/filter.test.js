5c2231f4df6d9dd5fc44b8a3a64a5f6b38cfe093
Timo Virtanen <timo.virtanen@tuni.fi>
2025-12-11 16:44:01 +0200

    filter tests

--- tests/filter.test.js
+++ tests/filter.test.js
@@ -3,8 +3,69 @@ const { expect } = chai;
 
 import filter from "../library/src/filter.js";
 
-describe("Integration – Stub Tests", () => {
-  it("placeholder integration test", () => {
-    expect("ok").to.equal("ok");
+/**
+ * Filter objects in array
+ *
+ * Instead of returning empty array, the function returns
+ * Array containing empty array. 
+ * 
+ * 
+ * This error is overlooked in "Do not modify array itself",
+ * This is done so original Array can be checked that its not modified
+ * after using filter().
+ */
+
+
+describe("Filter", () => {
+  const users = [{ 'user': 'barney', 'active': true },{ 'user': 'fred',   'active': false },{ 'user': 'Harold', 'active': false },]
+  const users_true = [{ 'user': 'barney', 'active': true },{ 'user': 'fred',   'active': true },{ 'user': 'Harold', 'active': true },]
+  const users_false = [{ 'user': 'barney', 'active': false },{ 'user': 'fred',   'active': false }, { 'user': 'Harold', 'active': false },]
+  const users_long = [{ 'user': 'barney', 'active': false, 'status': 'online', 'age': null }]
+
+  it("filter users", () => {
+    expect(filter(users, ({ active }) => active))
+  .to.deep.equal([{ user: "barney", active: true }]);
+
+    expect(filter(users, ({ active }) => !active))
+  .to.deep.equal([{ user: "fred", active: false }, { user: 'Harold', 'active': false }]);
   });
-});
+
+  it("filter Active", () => {
+    expect(filter(users_true, ({ active }) => active))
+  .to.deep.equal(users_true);
+
+    expect(filter(users_true, ({ active }) => !active))
+  .to.deep.equal([]);
+  });
+    
+  it("filter non-active", () => {
+    expect(filter(users_false, ({ active }) => !active))
+  .to.deep.equal(users_false);
+
+    expect(filter(users_false, ({ active }) => active))
+  .to.deep.equal([]);
+  });
+
+  // Should return empty array, not array containing empty array
+  it("filter empty, return empty Array", () => {
+    expect(filter([], ({ active }) => active))
+  .to.deep.equal([]);
+  });
+
+  it("Different Array structure", () => {
+    expect(filter(users_long, ({ active }) => !active))
+  .to.deep.equal(users_long);
+  });
+
+  it("Do not modify array itself", () => {
+    expect(filter(users_true, ({ active }) => active))
+  .to.deep.equal(users_true);
+
+    expect(filter(users_true, ({ active }) => !active))
+  .to.deep.equal([[]]);
+
+    // Should return original value
+      expect(filter(users_true, ({ active }) => active))
+  .to.deep.equal(users_true);
+  });
+})
