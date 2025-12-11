50fdce6bcb60f68b0f0b865710b3792928431ecf
Timo Virtanen <timo.virtanen@tuni.fi>
2025-12-11 15:46:03 +0200

    eq unit tests

--- tests/eq.test.js
+++ tests/eq.test.js
@@ -2,9 +2,46 @@ import chai from "chai";
 const { expect } = chai;
 
 import eq from "../library/src/eq.js";
-
-describe("Integration – Stub Tests", () => {
-  it("placeholder integration test", () => {
-    expect("ok").to.equal("ok");
+/**
+ * Compare two items.
+ * 
+ * According to documentation:
+ *  * eq('a', Object('a'))
+ * // => false
+ *
+ * but comparisons between numbers and strings fail.
+ *
+ *
+ */
+describe("Integration Stub Tests", () => {
+  it("Numbers", () => {
+    expect(eq(1,1)).to.equal(true);
+    expect(eq(-1,-1)).to.equal(true);
+    expect(eq(1.99,1.99)).to.equal(true);
+    expect(eq(-2.5,-2.5)).to.equal(true);
+    expect(eq(-2.5,2.5)).to.equal(false);
+    expect(eq(2.5,-2.5)).to.equal(false);
   });
+
+  it("Strings", () => {
+    expect(eq("Hello","Hello")).to.equal(true);
+    expect(eq("hello","Hello")).to.equal(false);
+    expect(eq("1","1")).to.equal(true);
+    expect(eq("-2.5","2.5")).to.equal(false);
+  });
+
+  // Fails
+  it("Compare strings and integers", () => {
+    expect(eq(2.5,"2.5")).to.equal(false);
+    expect(eq("2.5",2.5)).to.equal(false);
+    expect(eq(-1.99,"-1.99")).to.equal(false);
+    expect(eq("-2.5","2.5")).to.equal(false);
+  });
+
+  it("Compare NaN types.", () => {
+    expect(eq(NaN,NaN)).to.equal(true);
+    expect(eq(null,null)).to.equal(true);
+    expect(eq(NaN,null)).to.equal(false);
+  });
+
 });
