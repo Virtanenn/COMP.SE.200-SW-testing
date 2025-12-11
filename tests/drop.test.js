e160eaf3aa7e041ee657a6a2aafc94d123421ad8
Timo Virtanen <timo.virtanen@tuni.fi>
2025-12-11 15:21:27 +0200

    drop test

--- tests/drop.test.js
+++ tests/drop.test.js
@@ -2,9 +2,32 @@ import chai from "chai";
 const { expect } = chai;
 
 import drop from "../library/src/drop.js";
-
-describe("Integration – Stub Tests", () => {
-  it("placeholder integration test", () => {
-    expect("ok").to.equal("ok");
+/**
+ * Drop items from array.
+ * 
+ * Drop n elements from the beginning of an array
+ * All test pass
+ *
+ */
+describe("drop", () => {
+  it("drop first", () => {
+    expect(drop([1,2,3])).to.deep.equal([2,3]);
+    expect(drop([2,3,4])).to.deep.equal([3,4]);
+    expect(drop([4,3,2])).to.deep.equal([3,2]);
+  });
+  it("drop two", () => {
+    expect(drop([1,2,3],2)).to.deep.equal([3]);
+    expect(drop(["Hello",2,1],2)).to.deep.equal([1]);
+    expect(drop([3,2,"Hello"],2)).to.deep.equal(["Hello"]);
+  });
+  it("Drop all", () => {
+    expect(drop([1,2,3],3)).to.deep.equal([]);
+    expect(drop(["Hello",2,1],6)).to.deep.equal([]);
+    expect(drop([-4,2,-1,2,5,7,-3,3,-7,-1,-12,-3,2,1],100)).to.deep.equal([]);
+  });
+  it("Drop nothing", () => {
+    expect(drop([1,2,3],0)).to.deep.equal([1,2,3]);
+    expect(drop([-3,2,1],0)).to.deep.equal([-3,2,1]);
+    expect(drop([-3,"Hello",1],null)).to.deep.equal([-3,"Hello",1]);   
   });
 });
